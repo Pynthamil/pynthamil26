77d34664b0e4129bb09680844461a5ade97324b0
Pynthamil Pavendan <pyndu15@gmail.com>
2026-09-16 12:29:27 +0530

    Clean up temporary scripts

--- fix-italic.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('src/app/globals.css', 'utf8');
-css = css.replace(/font-style: normal;/g, '');
-fs.writeFileSync('src/app/globals.css', css);
-console.log("Removed font-style: normal from globals.css to avoid conflicts");
